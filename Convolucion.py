f7fbf0e52c87952d03e54b619f8db42ff1493fb0
GSLeonardo <leo_galindo_01@hotmail.com>
2020-09-17 10:40:50 -0500

    Kernel Canny Listo

--- Convolucion.py
+++ Convolucion.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('/home/leogalindo01/Documents/GitHub/Convolucion/horizon.jpg') #Transformar la imagen a numpy array
+img = cv2.imread('/home/leogalindo01/Documents/GitHub/Convolucion/Goku.jpeg') #Transformar la imagen a numpy array
 #cv2.namedWindow("Original", cv2.WINDOW_KEEPRATIO)
 #cv2.imshow("Original", img)#Mostrar imagen original
 #cv2.waitKey(0)
@@ -23,19 +23,21 @@ new_img = np.zeros(gray.shape)
 #kernel = [[1,0,-1],[2,0,-2],[1,0,-1]]
 #kernel = [[-1,-1,-1],[-1,8,-1],[-1,-1,-1]]
 #kernel = [[0.1,0.1,0.1],[0.1,0.1,0.1],[0.1,0.1,0.1]]
-for i in range(1, row-1):
-    for j in range(1, col-1):
+padding = 2
+for i in range(padding, row-padding):
+    for j in range(padding, col-padding):
         kernel = [[2,4,5,4,2],[4,9,12,9,4],[5,12,15,12,5],[4,9,12,9,4],[2,4,5,4,2]]#Kernel de la imagen
         count = 0
-        for y in range(3):
-            for x in range(3):  
-                kernel[y][x] *= gray[y+i-1][x+j-1]
+        for y in range(5):
+            for x in range(5):  
+                kernel[y][x] *= gray[y+i-padding][x+j-padding]
                 count += kernel[y][x]
+                count *= 1.0/159
         new_img[i][j] = count
 
 print(new_img)
-print(new_img[0][0])
 cv2.namedWindow("New", cv2.WINDOW_KEEPRATIO)
 cv2.imshow("New", new_img)#imagen con el nuevo filtro
+cv2.waitKey(0)
 cv2.waitKey(0)
 cv2.destroyAllWindows
